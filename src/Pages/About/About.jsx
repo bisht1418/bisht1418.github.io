8ab1cc2b45cc0591bcafd99f46b37e3305fd3a0d
Neeraj Bisht <bishtnrj1418@gmail.com>
2023-04-28 20:10:07 +0530

    correct tech word

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Heading, Text } from "@chakra-ui/react";
+import { Box, Container, Heading, Text, Flex } from "@chakra-ui/react";
 
 const About = () => {
   return (
@@ -7,14 +7,22 @@ const About = () => {
       <Heading pb="10px" lineHeight="tall">
         {`{ About Me }`}
       </Heading>
-      <Text id="user-detail-intro">
-        Hey! I'm Neeraj Singh Bisht., Solution driven Full Stack Web Developer
-        skilled in MERN Stack. Highly passionate about building web applications
-        and confident enough to learn new things with a curious mind, an
-        absolute passion for coding and the ability to write clean and efficient
-        code. Looking forward to taking up a challenging and responsible role
-        within an organization as a full-stack web developer.
-      </Text>
+
+      <Flex>
+        <Container>
+          <Text id="user-detail-intro">
+            Hey! I'm Neeraj Singh Bisht, Solution driven Full Stack Web
+            Developer proficient in React, Express, MongoDB, NodeJS, HTML, CSS
+            and JavaScript. Highly passionate about building web applications
+            and confident enough to learn new things with a curious mind, an
+            absolute passion for coding and the ability to write clean and
+            efficient code. Looking forward to taking up a challenging and
+            responsible role within an organization as a full-stack web
+            developer.
+          </Text>
+        </Container>
+      </Flex>
+
       <hr />
     </Box>
   );
